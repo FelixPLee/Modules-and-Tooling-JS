2547b1845341a52b82c159e4be7da5ea7c2f1f36
Felix <felixpinheirolee@gmail.com>
2025-10-07 12:34:18 -0300

    npm package

--- script.js
+++ script.js
@@ -96,3 +96,20 @@ console.log(shoppingCart2.shippingCoast)
 // }
 // // import
 // const {addToCart} = require(`./shoppingCart.js`)
+
+import cloneDeep from`./node_modules/lodash-es/cloneDeep.js`
+
+const state = {
+    cart: [
+        {product: `bread`, quantity: 5},
+        {product: `pizza`, quantity: 5},
+    ],
+    user: {loggedIn: true}
+}
+
+const stateClone = Object.assign({}, state)
+const stateDeepCLone = cloneDeep(state)
+state.user.loggedIn = false
+console.log(stateDeepCLone)
+console.log(stateClone)
+
